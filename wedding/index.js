03225764c509e53496b58a0870fa8ced391e92ed
Waters <h2oboi89@gmail.com>
2016-10-30 15:25:38 -0400

    Moved image to static folder

--- wedding/index.js
+++ wedding/index.js
@@ -9,8 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/AuGresMap.PNG', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-})
+app.use(express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
